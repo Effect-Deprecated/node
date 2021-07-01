f9d73117b636b925eda2a9fbdcc86767ad812e42
Michael Arnaldi <arnaldimichael@gmail.com>
2021-07-01 19:53:59 +0100

    chore(build): upgrade build

--- jest.config.js
+++ jest.config.js
@@ -10,8 +10,7 @@ module.exports = {
   setupFiles: ["./scripts/jest-setup.ts"],
   modulePathIgnorePatterns: [
     "<rootDir>/packages/.*/build",
-    "<rootDir>/packages/.*/compiler-debug",
-    "<rootDir>/_tmp"
+    "<rootDir>/packages/.*/dist"
   ],
   verbose: true,
   globals: {
