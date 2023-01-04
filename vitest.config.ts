861c135b2dd8d8712405e715a2813722b3e37072
Maxwell Brown <maxwellbrown1990@gmail.com>
2023-01-04 15:08:16 +0000

    convert to pnpm

---
+++ vitest.config.ts
@@ -0,0 +1,27 @@
+/// <reference types="vitest" />
+import { effectPlugin } from "@effect/vite-plugin"
+import path from "path"
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  plugins: [
+    effectPlugin({
+      babel: {
+        plugins: [["annotate-pure-calls"]],
+        compact: false
+      },
+      tsconfig: "tsconfig.test.json"
+    })
+  ],
+  test: {
+    include: ["./test/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    exclude: ["./test/utils/**/*.ts", "./test/**/*.init.ts"],
+    globals: true
+  },
+  resolve: {
+    alias: {
+      "@effect/node/test": path.resolve(__dirname, "/test"),
+      "@effect/node": path.resolve(__dirname, "/src")
+    }
+  }
+})
