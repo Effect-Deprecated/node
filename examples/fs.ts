b5aa94fc7f6bb799acf80e4a41f0332b23c77e59
Tim Smart <hello@timsmart.co>
2023-02-04 21:53:15 +1300

    chore: fix fs example

--- examples/fs.ts
+++ examples/fs.ts
@@ -1,5 +1,5 @@
 import * as Fs from "@effect/node/internal/fs"
-import { pipe } from "@fp-ts/data/Function"
+import { pipe } from "@fp-ts/core/Function"
 import * as Effect from "@effect/io/Effect"
 import * as Exit from "@effect/io/Exit"
 import * as Cause from "@effect/io/Cause"
@@ -7,8 +7,8 @@ import * as Stream from "@effect/stream/Stream"
 
 const program = pipe(Fs.stream(process.argv[2]), Stream.run(Fs.sink(process.argv[3])))
 
-Effect.unsafeRun(program, (exit) => {
+Effect.runCallback(program, (exit) => {
   if (Exit.isFailure(exit)) {
-    console.error(Cause.pretty()(exit.cause))
+    console.error(Cause.pretty(exit.cause))
   }
 })
