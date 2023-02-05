130fdc833aea26232e0cd1363a18aad2e9a8ca5b
Tim Smart <hello@timsmart.co>
2023-02-06 00:59:10 +1300

    fix: remove src/ from effect/io types

--- test/Runtime.ts
+++ test/Runtime.ts
@@ -1,10 +1,9 @@
 import * as Effect from "@effect/io/Effect"
 import * as _ from "@effect/node/Runtime"
-import * as it from "@effect/node/test/utils/extend"
 import { mockProcessExit } from "vitest-mock-process"
 
 describe("Runtime", () => {
-  it.it("runMain exit", () => {
+  it("runMain exit", () => {
     const mockExit = mockProcessExit()
     _.runMain(Effect.succeed("hi world"))
     expect(mockExit).toHaveBeenCalledWith(0)
