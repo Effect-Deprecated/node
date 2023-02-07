5528f0a3fd802f564a8276406387809399325065
Tim Smart <hello@timsmart.co>
2023-02-07 17:14:40 +1300

    chore: fix fs example

--- examples/runtime.ts
+++ examples/runtime.ts
@@ -1,6 +1,6 @@
+import * as Duration from "@effect/data/Duration"
 import * as Effect from "@effect/io/Effect"
 import * as RT from "@effect/node/Runtime"
 import { pipe } from "@fp-ts/core/Function"
-import * as Duration from "@fp-ts/data/Duration"
 
 pipe(Effect.fail("boom"), Effect.delay(Duration.minutes(1)), RT.runMain)
